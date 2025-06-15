a578bdafc821671b4c031450e5b7d3ed38a8dac1
Sunday <software.dev1988@gmail.com>
2025-06-16 01:07:17 +0300

    фикс функции подгрузки медиа-файлов

--- cinefrontend/src/utils/urls.ts
+++ cinefrontend/src/utils/urls.ts
@@ -1,6 +1,10 @@
 import { BASE_URL } from "../app";
 
 export function parseUrl(url: string): string {
+  if (!url) {
+    return '';
+  }
+
   if (
     /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/.test(
       url
